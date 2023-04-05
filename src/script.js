92ff4932c9e1236c63ceaa085220735d02593a35
James Holanda <jameshsousa@gmail.com>
2023-04-05 10:17:38 -0300

    Adding javascript into the project

---
+++ src/script.js
@@ -0,0 +1,34 @@
+// Daily
+
+const daily = document.getElementById("daily");
+daily.addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementsByClassName("daily").style.display = "block";
+    document.getElementsByClassName("weekly").style.display = "none";
+    document.getElementsByClassName("monthly").style.display = "none";
+ }
+
+
+
+
+//document.getElementsByClassName("daily")
+
+//element.style.property = new style
+
+//document.getElementById(id).style.property = new style
+
+//weekly
+
+//document.getElementsByClassName("weekly")
+
+//element.style.property = new style
+//document.getElementById(id).style.property = new style
+
+//monthly
+
+//document.getElementsByClassName("monthly")
+
+//element.style.property = new style
+
+//document.getElementById(id).style.property = new style
