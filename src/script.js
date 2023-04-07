bdfea1afcf4edffbeaa5645872dc93ea08260a4d
James Holanda <jameshsousa@gmail.com>
2023-04-07 09:23:51 -0300

    Organize part of content

--- src/script.js
+++ src/script.js
@@ -4,10 +4,10 @@ var daily = document.getElementsByClassName("daily");
     for (var i=0;i<daily.length;i+=1){
         daily[i].style.display = 'none';
     }
-var weekly = document.getElementsByClassName("weekly");
-    for (var i=0;i<weekly.length;i+=1){
-        weekly[i].style.display = 'none';
-    }
+// var weekly = document.getElementsByClassName("weekly");
+//     for (var i=0;i<weekly.length;i+=1){
+//         weekly[i].style.display = 'none';
+//     }
 var monthly = document.getElementsByClassName("monthly");
     for (var i=0;i<monthly.length;i+=1){
         monthly[i].style.display = 'none';
